0fce9221ad3e6207c56be2674b2b2cea2fd2c2d7
LyDat123 <lydat12a1@gmail.com>
2024-09-11 18:18:56 +0700

    edit question in preview

---
+++ deployment/native/styles.js
@@ -0,0 +1,15 @@
+import * as style0 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/administration/native/main";
+import * as style1 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/communitycommons/native/main";
+import * as style2 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/nanoflowcommons/native/main";
+import * as style3 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/webactions/native/main";
+import * as style4 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/atlas_core/native/main";
+import * as style5 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/feedbackmodule/native/main";
+import * as style6 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/builder/native/main";
+import * as style7 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/quizapp/native/main";
+import * as style8 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/extensions/native/main";
+import * as style9 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/themesource/anonymous/native/main";
+import * as style10 from "D:/0/New folder/quizapp-mx-course-829e53256f7109531dca52ddf298a337940febff/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8, style9, style10]);
